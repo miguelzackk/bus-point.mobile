34b2a17e17ea06d9ba1e1a146bc722f8d54a69af
lyanbrito <lyanbrito613@gmail.com>
2025-01-11 11:04:45 -0300

    feat:created-phrase-component

---
+++ src/pages/overboarding/components/phrase/index.jsx
@@ -0,0 +1,13 @@
+import SliderComponent from "./slider"
+import './styles.css';
+
+export default function PrhaseComponent() {
+    return (
+        <div className="phrase-wrapper">
+            <SliderComponent />
+            <p className="phrase">Chegue ao seu destino sem preocupações! </p>
+        </div>
+    )
+
+
+}   
